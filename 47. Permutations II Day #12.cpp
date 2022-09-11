ce71c074e776f5a3de1b3eda979a1bdbc6d0be4c
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-09-12 01:39:03 +0530

    Create 47. Permutations II Day #12.cpp

---
+++ 47. Permutations II Day #12.cpp
@@ -0,0 +1,42 @@
+   map<int,int> mp;
+    
+    vector<vector<int>> v;
+    
+    void combo(int i,vector<int> a,int n)
+    {
+        if(i==n)
+        {
+            v.push_back(a);
+            return;
+        }
+        
+        for(auto it:mp)
+        {
+            if(it.second==0)
+                continue;
+            
+            a.push_back(it.first);
+            
+            mp[it.first]--;
+            
+            combo(i+1,a,n);
+            
+            a.pop_back();
+            
+            mp[it.first]++;
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+      int n=nums.size();
+    mp.clear();
+        
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        vector<int> a;
+        combo(0,a,n);
+        
+        return v;
+    }
