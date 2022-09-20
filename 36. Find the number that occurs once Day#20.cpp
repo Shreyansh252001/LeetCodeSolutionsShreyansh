d564c0c71732b293791990fe9a31442f599c0e42
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-09-20 14:36:45 +0530

    Create 36. Find the number that occurs once Day#20.cpp

---
+++ 36. Find the number that occurs once Day#20.cpp
@@ -0,0 +1,12 @@
+int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        
+        int sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            sum=sum^nums[i];
+        }
+        
+        return sum;
+    }
