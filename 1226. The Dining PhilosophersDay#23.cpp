3e40c651c94d1710783886782802efabcaf5021d
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-09-22 23:17:55 +0530

    Update 1226. The Dining PhilosophersDay#23.cpp

--- 1226. The Dining PhilosophersDay#23.cpp
+++ 1226. The Dining PhilosophersDay#23.cpp
@@ -23,3 +23,30 @@ class DiningPhilosophers {
         }
     }
 }
+
+/*cpp file*/
+ mutex mtx;//A mutex is a lockable object that is designed to signal when critical 
+//sections of code need exclusive access, preventing other threads with 
+//the same protection from executing concurrently and access the same memory locations.
+public:
+    DiningPhilosophers() {
+       
+    }
+
+    void wantsToEat(int philosopher,
+                    function<void()> pickLeftFork,
+                    function<void()> pickRightFork,
+                    function<void()> eat,
+                    function<void()> putLeftFork,
+                    function<void()> putRightFork) 
+    {
+		mtx.lock();
+        
+        pickLeftFork();
+        pickRightFork();
+        eat();
+        putLeftFork();
+        putRightFork();
+        
+        mtx.unlock();
+    }
