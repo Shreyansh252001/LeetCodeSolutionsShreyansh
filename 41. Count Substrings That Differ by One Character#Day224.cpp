442a70639323e7eb2f089db0630de96ddf9d5370
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-09-25 22:41:29 +0530

    Create 41. Count Substrings That Differ by One Character#Day224.cpp

---
+++ 41. Count Substrings That Differ by One Character#Day224.cpp
@@ -0,0 +1,15 @@
+int countSubstrings(string &s, string &t) {
+    int res = 0;
+    for (int i = 0; i < s.size(); ++i)
+        for (int j = 0; j < t.size(); ++j) {
+            if (s[i] != t[j]) {
+                int l = 1, r = 1;
+                while (min(i - l, j - l) >= 0 && s[i - l] == t[j - l])
+                    ++l;
+                while (i + r < s.size() && j + r < t.size() && s[i + r] == t[j + r])
+                    ++r;
+                res += l * r;
+            }
+        }
+    return res;
+}
