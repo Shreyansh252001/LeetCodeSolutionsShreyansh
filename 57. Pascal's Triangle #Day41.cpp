6fd044f5f792ea2b2ba3817aca69bb2564c08a33
Shreyansh Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-10-12 01:36:52 +0530

    Create 57. Pascal's Triangle #Day41.cpp

---
+++ 57. Pascal's Triangle #Day41.cpp
@@ -0,0 +1,13 @@
+  vector<vector<int>> ans(numRows);
+        ans[0].push_back({1});
+        
+        for(int i=1;i<numRows;i++)
+        {
+            ans[i].push_back(1);
+            for(int j=1;j<=i-1;j++)
+            {
+                ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+            ans[i].push_back(1);
+        }
+        return ans;
