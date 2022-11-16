148d2613c4c58188cc750a698f263e713ca53a53
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-11-17 00:10:31 +0530

    Create 93. Find the majority element in an array_#day74.cpp

---
+++ 93. Find the majority element in an array_#day74.cpp
@@ -0,0 +1,13 @@
+int candidate = 0;
+        int count = 0;
+        for(int num:nums) {
+            if(count == 0) {
+                candidate = num;
+            }
+            if(num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
