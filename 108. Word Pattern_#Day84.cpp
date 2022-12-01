6dfdce417b7caded78ce29a0278bcb3e52658e62
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2022-12-02 01:24:11 +0530

    Create 108. Word Pattern_#Day84.cpp

---
+++ 108. Word Pattern_#Day84.cpp
@@ -0,0 +1,16 @@
+ map<char, int> p2i;
+    map<string, int> w2i;
+      
+    stringstream in(s);
+      
+    int i = 0, n = pattern.size();
+      
+    for (string word; in >> word; ++i) 
+    {
+        if (i == n || p2i[pattern[i]] != w2i[word])
+            return false;
+      
+        p2i[pattern[i]] = w2i[word] = i + 1;
+    }
+      
+    return i == n;   
