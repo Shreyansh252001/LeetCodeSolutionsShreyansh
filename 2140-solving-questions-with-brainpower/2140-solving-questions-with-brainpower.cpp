9ad9ec349e2891872ca7d68c632d927cddc83def
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2023-05-13 00:09:37 +0530

    Time: 396 ms (28.76%), Space: 121.6 MB (50.34%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+  /*  
+  Recursion
+  
+  int takenottake(vector<vector<int>> &questions,int i,int n)
+    {
+        if(i>=n)//out of range return zero
+            return 0;
+        
+        return max(questions[i][0]+takenottake(questions,i+questions[i][1]+1,n),takenottake(questions,i+1,n));
+        //if talking the current index add the points for it and go beyond the range i.e i+questions[i][1]+1 to the just next index after range
+                                //OR
+        //if not talking the current index just go to next index as you are free to go to the next index
+    }
+    
+    long long mostPoints(vector<vector<int>>& questions) 
+    {
+        int n=questions.size();
+        
+       return takenottake(questions,0,n); 
+    }
+    */
+    /*Memoization( 1-D dp)
+    */
+    
+    vector<long long int> dp;
+    
+    long long int n;
+    
+    long long int takenottake(vector<vector<int>> &questions,long long int i,long long int n)
+    {
+         if(i>=n)
+            return 0;
+         
+        if(dp[i]!=-1)
+            return dp[i];
+         
+        return dp[i]=max(questions[i][0]+takenottake(questions,i+questions[i][1]+1,n),takenottake(questions,i+1,n));
+    }
+    
+    long long mostPoints(vector<vector<int>>& questions)
+    {
+       n=questions.size();
+       
+       dp.resize(n,-1);
+       
+       return takenottake(questions,0,n); 
+    }
+    
+    //*/
+};
