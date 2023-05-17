2fd0d8294349d34af16247133f0fb771abcf071f
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2023-05-17 16:02:18 +0530

    Time: 278 ms (36.62%), Space: 118.9 MB (60.08%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -30,7 +30,7 @@ public:
         int n=0;
         
         ListNode* fwd=head;
-        
+        //talking out the total length of linklist
         while(fwd)
         {
             fwd=fwd->next;
@@ -39,22 +39,30 @@ public:
         
         fwd=head;
         
-        int g=n/2;
+        int g=n/2;//positioning the head to the start to go till the n/2 so that the linklist turns reverse from half to the end
         
         while(g--!=1)
         {
             fwd=fwd->next;
         }
-     
-        ListNode* rev=reverse(fwd);
-           
-        fwd=head;
+     //suppose list is 3->2->5->4->1->6 so we send the 5 to till end to be reversed
+        ListNode* rev=reverse(fwd);//rev pointer contains the start of the reversed list which is to be used to do addition with head from start
+        //rev pointer points to the start of reverse list i.e. end of list to till where both the forward and backward moving pointer collide
         
+           fwd=head;
+        //now again this fwd acts as a starting point from linklist and rev acts as a starting point from end of linklist
+        
+        /*list looks like (fwd)3->2->5<-4<-1<-6(rev)
+                                     |
+                                     V
+                                   NULL*/
+           
+     
         int sum=0;
         
         while(rev && fwd)
         {
-            cout<<fwd->val<<" "<<rev->val<<"\n";
+           
             
             sum=max(sum,rev->val+fwd->val);
             
