380c282e8eb8e983a121b8279283c69ffb6ab1ac
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2023-05-24 01:32:14 +0530

    Time: 38 ms (50.74%), Space: 19.7 MB (85.93%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,37 +1,230 @@
-class KthLargest {
-public:
-    int n;
+// class pq
+// {
+//     int sz;
+//     int curr;
+//     vector<int> arr;
     
-    priority_queue<int,vector<int>,greater<int>> pq;
-    
-    KthLargest(int k, vector<int>& nums) {
-        n=k;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(pq.size()>=n)
-        {
-            if(pq.top()<nums[i])
-            { pq.pop();pq.push(nums[i]);}
+//     public:
+//         pq(int k)
+//         {
+//             sz=k;
             
+//             curr=-1;
+            
+//             arr.resize(k);
+//         }
+        
+//         void heapify(int idx)
+//         {
+//            int p=(idx-1)/2;
+           
+//            while(p>0 && arr[p]>arr[idx])
+//            {
+//                swap(arr[p],arr[idx]);
+               
+//                idx=p;
+               
+//                p=(idx-1)/2;
+//            }
+           
+//         }
+    
+//         void shiftdown(int i)
+//         {
+//             int lc=2*i+1;
+//             int rc=2*i+2;
+//             int maxi=i;
+
+//             if(lc<arr.size() && arr[maxi]>arr[lc])
+//             {
+//                 maxi=lc;
+//             }
+
+//             if(rc<arr.size() && arr[maxi]>arr[rc])
+//             {
+//                 maxi=rc;
+//             }
+
+//             if(maxi!=i)
+//             {
+//                 swap(arr[maxi],arr[i]);
+//                 shiftdown(maxi);
+//             }
+//         }
+    
+//         int top()
+//         {
+//             return arr[0];
+//         }
+    
+//         int pop()
+//         {
+//             int g=arr[0];
+            
+//             arr[0]=arr[curr];
+            
+//             curr--;
+            
+//             if(curr>=0)
+//                 shiftdown(0);
+            
+//             return g;
+//         }
+    
+//         void push(int a)
+//         {
+//             arr[++curr]=a;
+//             heapify(curr);
+//         }
+        
+//         int size()
+//         {
+//             return curr+1;
+//         }
+// };
+
+class KthLargest 
+{
+    
+public:
+       int size, maxSize;
+    vector<int> array;
+
+    void heapify_down(int parent) {
+        int smallest = parent;
+        int left = 2 * parent + 1;
+        int right = 2 * parent + 2;
+
+        if (left < size && array[left] < array[smallest])
+            smallest = left;
+
+        if (right < size && array[right] < array[smallest])
+            smallest = right;
+
+        if (smallest != parent) {
+            swap(array[parent], array[smallest]);
+            heapify_down(smallest);
         }
-        else
-           pq.push(nums[i]);
+    }
+
+    KthLargest(int k, vector<int>& nums) {        
+        this->maxSize = k;
+        
+        this->array = nums;
+        this->size = array.size();
+        
+        // last non leaf node;
+        int n = (size / 2) - 1;
+        
+        for (int i = n; i >= 0; i--) {
+            heapify_down(i);
+        }
+        while (size > k) { // delete until it is equal to k
+            swap(array[0], array[size - 1]);
+            size--;
+            heapify_down(0);
+            array.pop_back();
         }
     }
     
+    int add(int val) {
+        
+        size = array.size();
+        
+        if (size >= maxSize && val < array[0]) return array[0];
+        
+        else if (size >= maxSize)
+            array[0] = val;
+        else {
+            array.push_back(val);
+            size++;
+        }
+        
+        heapify_down(0);
+        return array[0];
+    }
+    
+    /*int n;
+    pq *obj;
+    
+     KthLargest(int k, vector<int>& nums) 
+     {
+         n=k;
+         
+        obj=new pq(k);
+         
+           for(int i=0;i<nums.size();i++)
+           {
+                if(obj->size()>=n)
+                {
+                    if(obj->top()>nums[i])
+                    {
+                        obj->pop();
+                        obj->push(nums[i]);
+                    }
+                }
+                else
+                  obj->push(nums[i]);
+           }
+        
+     }
+         
     int add(int val)
     {
-        if(pq.size()>=n)
+        cout<<"done"<<" "<<val<<" ";
+        
+        if(obj->size()>=n)
         {
-            if(pq.top()<val)
-            { pq.pop();pq.push(val);}
+            cout<<"overflow"<<" ";
             
+            if(obj->top() < val)
+            {
+              cout<<"exchange possible"<<" ";
+                
+                 obj->pop();
+                 obj->push(val);
+            }
         }
         else
-           pq.push(val);
+          obj->push(val);
         
-        return pq.top();
+        cout<<" "<<obj->top()<<"\n";
+        
+        return obj->top();
     }
+    */
+//     priority_queue<int,vector<int>,greater<int>> pq;
+    
+//     KthLargest(int k, vector<int>& nums) {
+//         n=k;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(pq.size()>=n)
+//         {
+//             if(pq.top()<nums[i])
+//             { pq.pop();pq.push(nums[i]);}
+            
+//         }
+//         else
+//            pq.push(nums[i]);
+//         }
+//     }
+    
+//     int add(int val)
+//     {
+//         if(pq.size()>=n)
+//         {
+//             if(pq.top()<val)
+//             { pq.pop();pq.push(val);}
+            
+//         }
+//         else
+//            pq.push(val);
+        
+//         return pq.top();
+//     }
+    
+    
 };
 
 /**
