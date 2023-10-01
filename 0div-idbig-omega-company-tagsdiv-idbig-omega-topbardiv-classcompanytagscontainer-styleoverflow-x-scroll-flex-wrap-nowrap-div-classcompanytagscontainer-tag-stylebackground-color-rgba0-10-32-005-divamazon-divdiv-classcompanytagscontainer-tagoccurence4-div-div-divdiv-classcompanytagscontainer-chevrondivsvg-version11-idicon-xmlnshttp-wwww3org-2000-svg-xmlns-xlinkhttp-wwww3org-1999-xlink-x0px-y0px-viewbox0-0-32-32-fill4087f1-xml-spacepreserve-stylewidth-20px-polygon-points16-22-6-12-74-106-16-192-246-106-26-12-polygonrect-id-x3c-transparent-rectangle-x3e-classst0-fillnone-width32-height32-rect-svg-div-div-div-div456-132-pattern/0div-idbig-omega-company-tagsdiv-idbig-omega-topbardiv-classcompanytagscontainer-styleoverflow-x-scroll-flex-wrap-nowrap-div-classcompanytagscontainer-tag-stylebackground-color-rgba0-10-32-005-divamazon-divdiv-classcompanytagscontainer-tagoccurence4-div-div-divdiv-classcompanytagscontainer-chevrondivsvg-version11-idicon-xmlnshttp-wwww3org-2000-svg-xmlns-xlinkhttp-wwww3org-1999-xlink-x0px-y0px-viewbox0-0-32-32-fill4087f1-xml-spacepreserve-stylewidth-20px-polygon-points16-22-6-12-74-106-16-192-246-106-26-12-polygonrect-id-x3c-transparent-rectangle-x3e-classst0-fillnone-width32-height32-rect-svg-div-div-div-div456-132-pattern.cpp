68e1d6222b5053afc1f89ddc82efaf0ced67b7f1
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2023-10-01 09:19:47 +0530

    Time: 75 ms (28.27%), Space: 49 MB (33.38%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         
-        int s3 = INT_MIN;
+       /* int s3 = INT_MIN;
         stack<int> st;
         for( int i = nums.size()-1; i >= 0; i -- ){
             if( nums[i] < s3 ) return true;
@@ -11,8 +11,51 @@ public:
             }
             st.push(nums[i]);
         }
+        */
+        
+        /*
+        O(N^2)
+        
+        reason for starting from right to left
+        
+        https://youtu.be/RZXxX1EU364?si=VH9nMjZldu_uSjrm&t=1071
+        */
+        int n=nums.size();
+        
+        int numsmid=INT_MIN;
+        
+        stack<int> st;
+        
+        st.push(nums[n-1]);
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            
+            while(!st.empty() && nums[i]>st.top())
+            {
+                numsmid=st.top();
+                
+                //taking out all the smaller elements and automatically assigning them to middle element
+                
+                //and then extitng the while loop
+                
+                st.pop();
+            }
+            
+            // in the above while loop the middle element automatically gets chosen from the right side of the array , 
+            
+            // and the below if condition check for the if while loop was not entered then is the current middle element gretaer then it's left element so that if so then return the answer as true. as the greatest element is already found and pused into the stack using st.push(nums[i]) in some of the previoous iterations
+            
+            
+            if(numsmid>nums[i])
+            {
+                return true;
+            }
+            
+            
+            st.push(nums[i]);
+        }
         
         return false;
-    
     }
 };
