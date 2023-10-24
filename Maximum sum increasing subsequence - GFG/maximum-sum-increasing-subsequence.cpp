5675abd7d2488b4b7f03af20350b1b6ce5cde9f5
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2023-10-24 13:04:08 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -7,42 +7,63 @@ class Solution{
 		
 
 	public:
-	int tnt(int arr[],int i,int n,int prevval)
-	{
-	    if(i==n)
-	        return prevval;
-	        
-	   int maxi=0;
-	   
-	 
-	        if(arr[i]>prevval)
-	        {
-	            maxi=max({maxi,arr[i]+tnt(arr,i+1,n,arr[i]),tnt(arr,i+1,n,prevval)});         
-	        }
-	        else
-	            maxi=max({maxi,tnt(arr,i+1,n,arr[i])});
-	    
-	    
-	    return maxi;
-	}
+
 	
 	int maxSumIS(int arr[], int n)  
 	{  
 	    // Your code goes here
 	    //return tnt(arr,0,n,0);
 	    
-	       int ans[n];
+	    vector<int> dp(n);
+	    
+	    int ans=0;
+	    
+	    dp[0]=arr[0];
+	    
+	    for(int i=1;i<n;i++)
+	    {
+            int maxx=0;
+            dp[i]=arr[i];
+            
+            for(int j=0;j<i;j++)
+            {
+                if(arr[i]>arr[j] && dp[j]>maxx)// the current i should be greater than
+                //all previous j's and dp[j] means all addition tiil jth index which is stored in dp[j]
+                //we have to select the maximum addition of all the previous j provided that current arr[j]<arr[i]
+                {
+                    maxx=dp[j];
+                }
+            }
+            
+            dp[i]+=maxx;
+	    }
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        if(dp[i]>ans)
+	        {
+	            ans=dp[i];
+	        }
+	    }
+	    
+	    return ans;
+	    
+	    
+	/*    
+	int ans[n];
 	       
     ans[0]=arr[0];
     
     for(int i=1;i<n;i++)
     {
         int maxx=0;
+        
         for(int j=0;j<i;j++)
         {
             if(arr[j]<arr[i] and ans[j]>maxx)
                  maxx=ans[j];
         }
+        
         ans[i]=arr[i]+maxx;
     }
     
@@ -56,7 +77,8 @@ class Solution{
             maxx=ans[i];
         }
     }
-    return maxx;
+    return maxx;*/
+    
 	}  
 };
 
