4e9e56d8a07045ae9ac632a3074edfe2fda2932c
Shreyansh Alokkumar Shukla <51321665+Shreyansh252001@users.noreply.github.com>
2023-10-31 23:06:15 +0530

    Time: 31 ms (18.20%), Space: 13.1 MB (40.57%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -5,6 +5,71 @@ public:
     int MOD = 1e9 + 7;
     int arrLen;
     
+    /*int dp(int curr, int remain) 
+    {
+        if (remain == 0) 
+        {
+            if (curr == 0) 
+            {
+                return 1;
+            }
+            
+            return 0;
+        }
+        
+        if (memo[curr][remain] != -1) {
+            return memo[curr][remain];
+        }
+
+        int ans = dp(curr, remain - 1);
+        if (curr > 0) {
+            ans = (ans + dp(curr - 1, remain - 1)) % MOD;
+        }
+        
+        if (curr < arrLen - 1) {
+            ans = (ans + dp(curr + 1, remain - 1)) % MOD;
+        }
+        
+        memo[curr][remain] = ans;
+        return ans;
+    }*
+    
+    int dp(int start,int steps)
+    {
+        if(start<0 || start>=n)
+            return 0;
+        
+        if(steps==0)
+        {
+            return start==0?1:0;
+        }
+        
+        if(memo[start][steps]!=-1)
+            return memo[start][steps];
+        
+        return memo[start][steps]=(dp(start-1,steps-1)%MOD+dp(start+1,steps-1)%MOD+dp(start,steps-1)%MOD);
+        
+    }
+    
+    int numWays(int steps, int arrLen) {
+       * arrLen = min(arrLen, steps);
+        this->arrLen = arrLen;
+        memo = vector(arrLen, vector(steps + 1, -1));
+        return dp(0, steps);*
+        
+        arrLen=min(arrLen,steps);
+        
+        n=arrLen;
+        
+        memo.resize(arrLen,vector<int>(2*steps+1,0));
+        
+        //(start,steps)
+        return dp(0,steps);
+    }*/
+
+    
+ 
+    
     int dp(int curr, int remain) {
         if (remain == 0) {
             if (curr == 0) {
@@ -19,6 +84,7 @@ public:
         }
 
         int ans = dp(curr, remain - 1);
+        
         if (curr > 0) {
             ans = (ans + dp(curr - 1, remain - 1)) % MOD;
         }
